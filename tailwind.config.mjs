61a88f6e85b34c296ab2dadee17fa5947a4c701e
Ella Mae <ellalala.mc@gmail.com>
2023-10-30 16:07:14 +0800

    Add Bahnschrift and Schabo Condensed in theme.fontFamily

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'sans': ['bahnschrift'],
+			'display': ['schabo_condensed'],
+		}
 	},
 	plugins: [],
 }
