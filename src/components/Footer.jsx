d66a4f358d9c011714c2aeda6fdced16cbb7e43c
Ella Mae <ellalala.mc@gmail.com>
2023-11-05 17:29:20 +0800

    Edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,32 +8,35 @@ function Footer(props) {
                     <div className="flex w-[571px] max-w-full items-start gap-3.5 ml-6 self-start max-md:flex-wrap">
                         <img
                             loading="lazy"
-                            src="/img/mmbu23_logo.png"
-                            className="aspect-[1.1] object-contain object-center w-[109px] overflow-hidden self-stretch max-w-full"
+                            src="/img/mmbulogo_highexposure.png"
+                            className="aspect-[1.1] object-contain object-center w-[65px] overflow-hidden self-stretch max-w-full"
                             alt="Logo"
                         />
-                        <h1 className="text-zinc-100 text-3xl font-semibold max-w-[452px] self-center grow shrink-0 basis-auto my-auto max-md:max-w-full">
+                        <h1 className="text-zinc-100 text-lg font-semibold max-w-[452px] self-center grow shrink-0 basis-auto my-auto max-md:max-w-full">
                             The Search for <br /> Mr. and Ms. Bicol University 2023
                         </h1>
                     </div>
-                    <p className="text-stone-300 text-base w-[622px] max-w-full ml-6 mt-5 self-start">
+                    <p className="text-stone-300 text-xs w-[622px] max-w-full ml-6 mt-5 self-start">
                         Presented by the Bicol University - University Student Council, MMBU returns this 2023 pulling out all the stops to bring you a show that's bigger, bolder, and avant-garde than ever before.
                     </p>
                     <hr className="bg-zinc-400 self-stretch w-full h-px mt-7 max-md:max-w-full" />
                     <div className="self-center flex w-full max-w-[1025px] items-start justify-between gap-5 mt-4 max-md:max-w-full max-md:flex-wrap max-md:justify-center">
-                        <p className="text-stone-300 text-base max-w-[623px] grow shrink-0 basis-auto mt-2.5 self-start max-md:max-w-full">
+                        <p className="text-stone-300 text-xs max-w-[623px] grow shrink-0 basis-auto mt-2.5 self-start max-md:max-w-full">
                             This tabulation web app was developed for Bicol University - University Student Council <br /> by Google Developer Student Clubs Bicol University.
                         </p>
-                        <img
-                            loading="lazy"
-                            src="/img/usc_logo.png" className="aspect-[1.15] object-contain object-center w-[86px] bg-blend-lighten overflow-hidden self-stretch max-w-full"
-                            alt="Image 1"
-                        />
-                        <img
-                            loading="lazy"
-                            src="/img/gdsc_logover2.png" className="aspect-[1.0] object-contain object-center w-[87px] overflow-hidden self-center max-w-full my-auto"
-                            alt="Image 2"
-                        />
+
+                        <div className="flex flex-row gap-1 items end max-w-[1025px]">
+                            <img
+                                loading="lazy"
+                                src="/img/usc_logo.png" className="aspect-[1.15] object-contain object-center w-[60px] bg-blend-lighten overflow-hidden self-stretch max-w-full"
+                                alt="BU-USC Logo"
+                            />
+                            <img
+                                loading="lazy"
+                                src="/img/gdscbu_logo.png" className="aspect-[1.0] object-contain object-center w-[59px] overflow-hidden self-center max-w-full my-auto"
+                                alt="GDSC BU Logo"
+                            />
+                        </div>
                     </div>
                 </div>
             </header>
